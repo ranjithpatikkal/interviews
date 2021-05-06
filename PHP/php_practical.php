145645914b8d596ab49445d66d26aee5a5e01221
sanathmadiyal <sanathmadiyal@yahoo.com>
2021-05-06 16:49:18 +0530

    changes

--- PHP/php_practical.php
+++ PHP/php_practical.php
@@ -1,4 +1,5 @@
 // find largent eliment from array
+1)
 
 function multimax( $array ) {
     foreach( $array as $value ) {
@@ -25,4 +26,143 @@ function multimax( $array ) {
 $arr= array(array(141,151,161),2,3,array(101,202,array(303,404,600)));
 $max = multimax($arr);
 var_dump($max);
+
+2) checkoutput
+
+$x = 9;
+echo $x; //9;
+echo "<br />";
+echo $x+++$x+++$x+++$x+++$x++; //19
+// 10+11+9
+// 10+11+12+9
+// 10+11+12+13+9
+
+
+echo "<br />";
+echo $x; //11
+echo "<br />";
+echo $x---$x--;// 11 -10 1
+echo "<br />";
+echo $x; //9
+echo "<br />";
+echo"=============================";
+
+3) value by reference 
+
+$a = 'abc';
+$b="mn";
+$b = &$a;
+$b = "21$a";
+
+echo"value ".$b;
+echo "<br />";
+echo$a;
+
+4) var_dump(0123 == 123); //false
+var_dump('0123' == 123); //true
+var_dump(0123 === 123); //false
+
+5) 
+$referenceTable = array();
+$referenceTable['val1'] = array(1, 2);
+$referenceTable['val2'] = 3;
+$referenceTable['val3'] = array(4, 5);
+
+$testArray = array();
+$testArray = array_merge($testArray, $referenceTable['val1']);
+var_dump($testArray);
+
+$testArray = array_merge($testArray, $referenceTable['val2']);
+var_dump($testArray);
+
+$testArray = array_merge($testArray, $referenceTable['val3']);
+var_dump($testArray);
+
+6)
+
+$numbers=array(12,23,45,20,5,6,34,17,9,56);
+$n=count($numbers);
+ for ($c = 0 ; $c < ( $n - 1 ); $c++)
+  {
+    for ($d = 0 ; $d < ($n - $c - 1); $d++)
+    {
+      if ($numbers[$d] > $numbers[$d+1]) /* For decreasing order use < */
+      {
+        $swap = $numbers[$d];
+        $numbers[$d]   = $numbers[$d+1];
+        $numbers[$d+1] = $swap;
+      }
+    }
+  }
+ 
+echo "Sorted list in ascending order <br />";
+ 
+  for ( $c = 0 ; $c < $n ; $c++ )
+     echo $numbers[$c]." ";
+
+7) Reverse Number
+
+$number = 12326;  
+$sum = 0;  
+while(floor($number)) {  
+  $rem = $number % 10;  
+  $sum = $sum * 10 + $rem; 
+  $number = $number/10; 
+   
+}  
+echo$sum;
+echo"<br>";
+
+8) Prime Number
+
+$count = 0;  
+$num = 2;  
+
+while ($count < 3 )  
+{  
+  $div_count=0;  
+  for ( $i=1; $i<=$num; $i++)  
+  {  
+    if (($num%$i)==0)  
+    {  
+      $div_count++;  
+    }  
+  }  
+
+  if ($div_count<3)  
+  {  
+      echo $num." , ";  
+      $count=$count+1;  
+  }  
+    $num=$num+1;  
+  }  
+
+9) Heighest number from array
+
+function multimax( $array ) {
+    foreach( $array as $value ) {
+        if( is_array($value) ) {
+            $subvalue = multimax($value);
+
+            // if the returned $subvalue is greater than our current highest value,
+            // set it as our $return value.
+            if( $subvalue > $return ) {
+                $return = $subvalue;
+            }
+
+        } elseif($value > $return) {
+            // ... $value is not an array so set the return variable if it's greater
+            // than our highest value so far.
+            $return = $value;
+        }
+    }
+
+    // return (what should be) the highest value from any dimension.
+    return $return;
+}
+
+$arr= array(array(141,151,161),2,3,array(101,202,array(303,404,600)));
+$max = multimax($arr);
+var_dump($max);
+
 
--- PHP/php_theory.php
+++ PHP/php_theory.php
@@ -1,11 +1,80 @@
-1) Regular Expression Functions
+
+1) Error types in php
+   https://phoenixnap.com/kb/php-error-types#ftoc-heading-4
+	a. Warning Error
+	b. Notice Error
+	c. Parse Error
+	d. Fatal Error
+2) enable errors
+	php script : ini_set('display_errors', 1);
+				 ini_set('display_startup_errors', 1);
+				 error_reporting(E_ALL); (E_NOTICE, E_WARNING, E_PARSE, E_ERROR)
+
+	php.ini : display_errors = on
+	.htaccess : php_flag display_errors 1
+
+3) include vs require
+	https://www.w3schools.com/php/php_includes.asp
+
+	It is possible to insert the content of one PHP file into another PHP file (before the server executes it), with the include or require statement.
+
+	The include and require statements are identical, except upon failure:
+
+	require will produce a fatal error (E_COMPILE_ERROR) and stop the script
+	include will only produce a warning (E_WARNING) and the script will continue 
+4) include_once and require_once : checks  If the file was already included previously, this statement 		will not include it again.
+
+
+5) Handling Files
+	readfile("webdictionary.txt")
+	fopen("webdictionary.txt", "r") https://www.w3schools.com/php/php_file_open.asp
+	fclose()
+
+6) Session:Session is nothing But it maintaine the User Information on Sevr Side,
+	For Examples When ever Client make request to the Application that time server will be Allocated block Of memory this Memory is called Session memory. The Session is not going to be visible via the client unless you explicitly.
+	Session is used for Single Client.in Web Server ISAPI_Aspne.EXE.
+	
+	Cookies:Cookie is nothin But Maintain Information on the Client machine .iT can be Two Types
+
+	1.InMemory(or)temparory
+	2.Parsistance :
+
+	https://www.guru99.com/cookies-and-sessions.html
+
+7) unlink vs unset 
+   unlink : used in filemanagement to delete file.
+   unset : make variable as undefined
+
+
+8) Regular Expression Functions
 
 https://www.w3schools.com/php/php_regex.asp
 
-2) Supper global variables
+9) Supper global variables
 https://www.w3schools.com/php/php_superglobals.asp
 
-3) PHP echo Vs print Statements
-https://www.w3schools.com/php/php_echo_print.asp
-4) String Functions
+10) PHP echo Vs print Statements
+
+11) constant vs define 
+https://www.tutorialspoint.com/compare-define-vs-const-in-php#:~:text=The%20basic%20difference%20between%20these,defines%20them%20at%20run%20time.&text=const%20accepts%20a%20static%20scalar,define()%20takes%20any%20expression.
+
+12) String Functions
 https://www.w3schools.com/php/php_string.asp
+
+613) Array Functions
+https://www.w3schools.com/php/php_ref_array.asp
+
+array_merge vs array_combine
+
+14) Array Types
+Indexed Arrays : The index can be assigned automatically (index always starts at 0)
+Associative Arrays : Associative arrays are arrays that use named keys that you assign to them.
+
+15) Sorting
+	sort() - sort arrays in ascending order
+	rsort() - sort arrays in descending order
+	asort() - sort associative arrays in ascending order, according to the value
+	ksort() - sort associative arrays in ascending order, according to the key
+	arsort() - sort associative arrays in descending order, according to the value
+	krsort() - sort associative arrays in descending order, according to the key
+
